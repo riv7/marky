36bf6f1fc40d5db5b6d53e1b0ae4ca5a8d483951
Wolfram Gulde <wolfram.gulde@exxcellent.de>
2016-11-08 21:17:46 +0100

    chore(Init): Initial commit with build setup

---
+++ source/app.js
@@ -0,0 +1,38 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+class Form extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {value: ''};
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) {
+    alert('Text field value is: ' + this.state.value);
+  }
+
+  render() {
+    return (
+      <div>
+        <input type="text"
+          placeholder="Hello!"
+          value={this.state.value}
+          onChange={this.handleChange} />
+        <button onClick={this.handleSubmit}>
+          Submit
+        </button>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(
+  <Form />,
+  document.getElementById('react-application')
+);
