ea2df5246ac865de2f8b0fa4b64222ffe67dd343
Wolfram Gulde <wolfram.gulde@exxcellent.de>
2016-11-16 22:47:43 +0100

    chore(Viewer): added react bootstrap (just a test)

--- source/components/components.js
+++ source/components/components.js
@@ -1,6 +1,36 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
+//Just a test
+import { Button } from 'react-bootstrap';
+import { ButtonToolbar } from 'react-bootstrap';
+
+const buttonsInstance = (
+  <ButtonToolbar>
+    {/* Standard button */}
+    <Button>Default</Button>
+
+    {/* Provides extra visual weight and identifies the primary action in a set of buttons */}
+    <Button bsStyle="primary">Primary</Button>
+
+    {/* Indicates a successful or positive action */}
+    <Button bsStyle="success">Success</Button>
+
+    {/* Contextual button for informational alert messages */}
+    <Button bsStyle="info">Info</Button>
+
+    {/* Indicates caution should be taken with this action */}
+    <Button bsStyle="warning">Warning</Button>
+
+    {/* Indicates a dangerous or potentially negative action */}
+    <Button bsStyle="danger">Danger</Button>
+
+    {/* Deemphasize a button by making it look like a link while maintaining button behavior */}
+    <Button bsStyle="link">Link</Button>
+  </ButtonToolbar>
+);
+
+
 export function GradeRow(props) {
   const {grade} = props;
   return <li>
@@ -72,6 +102,7 @@ export function FilterableStudentsTable(props) {
       <YearSelectionBar />
       <SearchBar />
       <StudentsTable studentsOfYear={studentsOfYear} />
-    </div>
+      {buttonsInstance}
+  </div>
   );
 }
