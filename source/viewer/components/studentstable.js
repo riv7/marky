6e19b152f35e6a9f16be1972dd1dff851f834b97
Wolfram Gulde <wolfram.gulde@exxcellent.de>
2017-02-20 10:33:51 +0100

    chore(presentation): added js presentation chapters until redux

--- source/viewer/components/studentstable.js
+++ source/viewer/components/studentstable.js
@@ -20,6 +20,27 @@ const StudentsTable = ({studentsOfYear}) => {
     return rows;
   }
 
+  const createStudentsTable2 = () => {
+    let rows = [];
+    studentsOfYear.forEach(gradeData => {
+      rows.push(
+        <GradeRow
+          grade={gradeData.get('grade')}
+          key={gradeData.get('id')}
+        />);
+
+      gradeData.get('students').forEach(student => {
+        rows.push(
+          <StudentRow
+            student={student}
+            key={student.get('id')}
+          />);
+      });
+    })
+    return rows;
+  }
+
+
   return (
     <div>
       <ListGroup>
