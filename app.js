6766da7730542e52f4c29bc0f137f7d00151c22d
Wolfram Gulde <wolfram.gulde@exxcellent.de>
2017-10-30 18:31:39 +0100

    chore(openshift): set default internal ip to 0.0.0.0, so openshif external routing seems to work ???!!!

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var gulp = require('gulp');
 var webserver = require('gulp-webserver');
 
 var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
-var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0'
 
 gulp.src('build/editor')
     .pipe(webserver({
