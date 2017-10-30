85f909d023180b8772cfde0bf6d3e12535c40d3e
Wolfram Gulde <wolfram.gulde@exxcellent.de>
2017-10-30 12:21:43 +0100

    chore(openshift): removed trailing slash

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var webserver = require('gulp-webserver');
 
 gulp.src('build/editor')
     .pipe(webserver({
-        host: 'marky-marky.192.168.99.100.nip.io/',
+        host: 'marky-marky.192.168.99.100.nip.io',
         port: 8080,
         livereload: true,
         fallback: 'index.html',
