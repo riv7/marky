83c85a4b103e3859f2429ef64d4bffb354100f1a
Wolfram Gulde <wolfram.gulde@exxcellent.de>
2017-10-30 12:44:51 +0100

    chore(openshift): tried to add host and port from env

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 var gulp = require('gulp');
 var webserver = require('gulp-webserver');
 
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+
 gulp.src('build/editor')
     .pipe(webserver({
-        host: 'marky-marky.192.168.99.100.nip.io',
-        port: 8080,
+        host: server_ip_address,
+        port: server_port,
         livereload: true,
         fallback: 'index.html',
         directoryListing: false,
