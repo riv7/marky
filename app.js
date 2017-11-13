c0d70c1bae8376097408da5bcb4f4bb5d9b60d62
Wolfram Gulde <wolfram.gulde@exxcellent.de>
2017-11-13 16:05:56 +0100

    chore(openshift): added openshift namespace to dynamically adjust proxy forwarding

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var webserver = require('gulp-webserver');
 
 var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
 var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0'
+var openshift_namespace = process.env.OPENSHIFT_NAMESPACE || 'marky'
 
 gulp.src('build/editor')
     .pipe(webserver({
@@ -14,6 +15,6 @@ gulp.src('build/editor')
         open: true,
         proxies: [{
             source: '/api',
-            target: 'http://marky-server-marky.192.168.99.100.nip.io/api'
+            target: `http://marky-server-${openshift_namespace}.192.168.99.100.nip.io/api`
         }]
     }));
